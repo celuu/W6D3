a0b38a54e9a2611df5fd0ff7ecc70f12cf471039
celuu <christine.e.luu@gmail.com>
2022-11-09 17:02:11 -0800

    finished First Routes

--- routes 2/app/controllers/users_controller.rb
+++ routes 2/app/controllers/users_controller.rb
@@ -1,17 +1,35 @@
 class UsersController < ApplicationController
     def index
         render json: User.all
-
     end
 
     def create
         user = User.new(params.require(:user).permit(:name, :email))
-        user.save!
-        render json: user
+        if user.save
+            render json: user
+        else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        user = User.find(params[:id])
+        render json: user
     end
 
+    def update
+        users = User.new(params.require(:user).permit(:name, :email))
+        user = user.find_by(id:params[:id])
+        if user.update(users)
+            redirect_to user_url(user.id)
+        else
+            render json: user.errors.full_messages, status:422
+        end
+    end
+
+    def destroy
+        user = User.find_by(id:params[:id])
+        user.destroy
+        redirect_to users_url
+    end
 end
