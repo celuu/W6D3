d0a2edd655ebf001674ecf318fd6f07c48734106
celuu <christine.e.luu@gmail.com>
2022-11-09 16:25:44 -0800

    Finished part 2

--- routes 2/app/controllers/users_controller.rb
+++ routes 2/app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
-class UserController < ApplicationController
-    
+class UsersController < ApplicationController
+    def index
+        render plain: "i'm in the index action!"
+    end
+
+    def create
+        render json: params
+    end
+
+    def show
+        render json: params
+    end
+
 end
